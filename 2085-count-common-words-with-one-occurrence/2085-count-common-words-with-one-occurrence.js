dd4a8d5576c6766dc82dce7ef351242d04c3122d
Akshaymon K V <kvakshaymon@gmail.com>
2022-10-15 17:49:56 +0530

    Time: 114 ms (70.73%), Space: 44.4 MB (98.48%) - LeetHub

---
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {number}
+ */
+var countWords = function (words1, words2) {
+  count=0
+words1.forEach(element => {
+    if((words1.indexOf(element)==words1.lastIndexOf(element))&&(words2.includes(element)==true)){   
+        if((words2.indexOf(element)==words2.lastIndexOf(element))){
+            count+=1
+        }
+    }
+})
+    return count
+}
