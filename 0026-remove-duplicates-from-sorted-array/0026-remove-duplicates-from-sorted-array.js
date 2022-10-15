424cb9163a97c0aa9313e44003a0d21b05ac0bd4
Akshaymon K V <kvakshaymon@gmail.com>
2022-10-15 17:52:36 +0530

    Time: 119 ms (63.94%), Space: 45 MB (21.54%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let a=0;
+    for(i=0;i<nums.length;i++){
+        if(nums[i]!=nums[i-1]){
+            nums[a] = nums[i];
+            a+=1
+        }
+    }
+    return a
+};
