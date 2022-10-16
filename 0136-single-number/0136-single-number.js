db0475a116df72892e57b6c715ec2705d17a924a
Akshaymon K V <kvakshaymon@gmail.com>
2022-10-16 15:42:49 +0530

    Time: 1222 ms (5.00%), Space: 42.9 MB (91.91%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+   nums.forEach(element => {
+    if((nums.indexOf(element)==nums.lastIndexOf(element)))
+       a = element 
+})
+   return a
+};
